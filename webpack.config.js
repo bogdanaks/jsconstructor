6fbd50fa1afa2de445e1c6eb83479ba04f829284
bogdanaks <bogdanaks@bk.ru>
2020-08-30 10:42:40 +0300

    Настроил базовый конфиг

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        port: 7777,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+    ],
+}
